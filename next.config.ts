610efaa23dfaec9ea91ec4619681f28bb35095bb
Shravan Viradiya <95079227+skviradiya@users.noreply.github.com>
2024-11-29 16:53:38 +0530

    changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true, // Necessary if you use Next.js image component
+  },
+  basePath: '/skviradiya',
+  assetPrefix: '/skviradiya',
 };
 
-export default nextConfig;
+module.exports = nextConfig;
