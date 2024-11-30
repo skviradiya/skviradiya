c2ed2a006d9d1dc359a25aa4766e4d1a61c80486
Shravan Viradiya <95079227+skviradiya@users.noreply.github.com>
2024-11-30 10:53:13 +0530

    some changes

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,9 @@ const nextConfig: NextConfig = {
   },
   basePath: '/skviradiya',
   assetPrefix: '/skviradiya/',
+  env: {
+    basePath: process.env.basePath,
+  },
 };
 
 module.exports = nextConfig;
