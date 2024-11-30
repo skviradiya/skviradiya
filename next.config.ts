621ebb093a30a699f8cd1d55f56b3aa03db3b913
Shravan Viradiya <95079227+skviradiya@users.noreply.github.com>
2024-11-30 10:00:20 +0530

    remove yml file

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   // reactStrictMode: true,
-  // images: {
-  //   unoptimized: true, // Necessary if you use Next.js image component
-  // },
+  images: {
+    unoptimized: true, // Necessary if you use Next.js image component
+  },
   basePath: '/skviradiya',
   assetPrefix: '/skviradiya',
 };
