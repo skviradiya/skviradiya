f3633b687a329b917aa0009055d50456c5743bec
Shravan Viradiya <95079227+skviradiya@users.noreply.github.com>
2024-12-03 21:17:01 +0530

    Revert "some changes for image issue solve"
    This reverts commit bcd5c34e4e6f6ef76a9870d2145c51648f3f1a92.

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -5,19 +5,57 @@ import { motion } from "framer-motion";
 import { ProjectCard } from "@/components/project-card";
 import { AnimatedBackground } from "@/components/animated-background";
 import { Nav } from "@/components/nav";
-import { Project, projects } from "@/data/projects";
+import { projects } from "@/data/projects";
 
-export async function getStaticProps() {
-  const basePath = process.env.BASE_PATH || "";
-  return {
-    props: {
-      projects: projects.map((img) => ({
-        ...img,
-      })),
-    },
-  };
-}
-export default function ProjectsPage({ projects }: { projects: Project[] }) {
+// const projects = [
+//   {
+//     title: "Birla Opus ID",
+//     description:
+//       "A contractor portal app for Aditya Birla Group with features like Adobe Analytics integration, Clever Tap integration, and advanced QR scanning capabilities.",
+//     image: "/assets/images/birla-opus-id.webp?height=400&width=600",
+//     link: "/projects/birla-opus",
+//     playStoreLink:
+//       "https://play.google.com/store/apps/details?id=com.birlaopusid.contractorportal",
+//   },
+//   {
+//     title: "Nettpage",
+//     description:
+//       "A mobile application that combines e-commerce and social media features, allowing users to browse, purchase, and sell products while interacting with other users.",
+//     image: "/assets/images/nettpage.webp?height=400&width=600",
+//     link: "/projects/nettpage",
+//     playStoreLink:
+//       "https://play.google.com/store/apps/details?id=com.app.nettpage",
+//   },
+
+//   {
+//     title: "TipOff",
+//     description:
+//       "A word guessing game app featuring video conference gameplay capabilities.",
+//     image: "/assets/images/tipoff.png?height=400&width=600",
+//     link: "/projects/tipoff",
+//     playStoreLink:
+//       "https://apps.apple.com/us/app/tipoff-word-guessing-game/id1110306961",
+//   },
+//   {
+//     title: "WeSmoke",
+//     description:
+//       "A social media app combining various features such as podcasting, chatting, and media posting.",
+//     image: "/assets/images/wesmoke.png?height=400&width=600",
+//     link: "/projects/wesmoke",
+//     playStoreLink: "https://apps.apple.com/us/app/wesmoke/id1577787304",
+//   },
+//   {
+//     title: "Satsang App",
+//     description:
+//       "An app for managing and analyzing large data sets with comprehensive CRUD operations.",
+//     image: "/assets/images/satsang.webp?height=400&width=600&resize=true",
+//     link: "/projects/satsang",
+//     playStoreLink:
+//       "https://play.google.com/store/apps/details?id=com.baps_yuva",
+//   },
+// ];
+
+export default function ProjectsPage() {
   return (
     <div className="min-h-screen bg-black text-white">
       <AnimatedBackground />
