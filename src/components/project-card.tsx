7cb7610d68cd033ecbe01bb5351ed9a72f81712b
Shravan Viradiya <95079227+skviradiya@users.noreply.github.com>
2024-12-05 15:10:59 +0530

    handle base path changes

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -32,7 +32,7 @@ export function ProjectCard({
           <CardContent>
             <div className="relative aspect-video overflow-hidden rounded-lg">
               <Image
-                src={basePath + image}
+                src={"/skviradiya" + image}
                 alt={title}
                 fill
                 className="object-cover transition-transform duration-500 group-hover:scale-110"
