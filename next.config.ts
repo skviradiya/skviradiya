dbece045c4c870fe2cd406f3d6ffb46f727a2607
Shravan Viradiya <shravanviradiyakb@gmail.com>
2026-04-29 10:08:11 +0530

    setup deploy

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: process.env.BASE_PATH,
+  output: "export",
+  basePath: "/skviradiya",
+  assetPrefix: "/skviradiya/",
   images: {
     unoptimized: true,
-    
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
